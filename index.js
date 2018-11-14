21b13a5a548c7be8ac90b6ab34d8d9c5707fda92
marioamandio <30850605+marioamandio@users.noreply.github.com>
2018-11-14 19:49:19 +0000

    another try

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const findAddress = (address, cb) => {
 
 if(process.env.NODE_ENV === 'production') {
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
